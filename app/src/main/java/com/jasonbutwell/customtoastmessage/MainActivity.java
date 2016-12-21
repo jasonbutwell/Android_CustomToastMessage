52da8057cf75a05fac222e89db4106dacac46fee
Jason Butwell <jasonbutwell@gmail.com>
2016-12-21 23:23:56 +0000

    Created custom XML to use for a custom formatted Toast message. Uses a custom line of text, custom colours and a custom image.

--- app/src/main/java/com/jasonbutwell/customtoastmessage/MainActivity.java
+++ app/src/main/java/com/jasonbutwell/customtoastmessage/MainActivity.java
@@ -3,6 +3,10 @@ package com.jasonbutwell.customtoastmessage;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,7 +16,44 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        toastMessageCentered();
+        //toastMessageCentered();
+        customXMLToastMessage();
+    }
+
+    // A simple custom Toast message using an XML layout
+
+    private void customXMLToastMessage() {
+
+        // Inflate the layout
+
+        LayoutInflater inflater = getLayoutInflater();
+        View layout = inflater.inflate(R.layout.custom_toast_layout,(ViewGroup)findViewById(R.id.customToastLayout));
+
+        // set the text of the toast
+
+        TextView tv = (TextView)layout.findViewById(R.id.textContent);
+        tv.setText("This is a toast message");
+
+        // create a new toast
+
+        Toast toast = new Toast(getApplicationContext());
+
+        // set the duration
+
+        toast.setDuration(Toast.LENGTH_SHORT);
+
+        // set positioning
+
+        toast.setGravity(Gravity.BOTTOM | Gravity.LEFT, 100,100 );
+
+        // use our custom XML for the toast
+
+        toast.setView(layout);
+
+        // show the toast
+
+        toast.show();
+
     }
 
     // A simple custom Toast Message
